be8c31c26a45ec0234e3476c2faad4a58b13fe17
Sanna Balouchi <105402020+saunade@users.noreply.github.com>
2022-09-28 11:09:36 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,6 @@
+def count(num):#num is the variable name
+    print(num + 1) #num prints out to user with an added one value
+
+if __name__ == "__main__": #main function that we call our defined functions
+    for n in range(10): 
+        count(n) #makes the value of num n (the counter in the for loop)
