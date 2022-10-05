f641e91da8fd518197c507cf1fc2681099d32a0e
Sanna Balouchi <105402020+saunade@users.noreply.github.com>
2022-10-05 17:09:57 -0400

    Program for Lab 1

--- Lab1.py
+++ Lab1.py
@@ -1,4 +1,6 @@
 #grabbing user input to assign values to h,w,l
+#basic program
+
 height = int(input("Enter the height of the rectangular prism: "))
 width = int(input("Enter the width of the rectangular prism: "))
 length = int(input("Enter the length of the rectangular prism: "))
